376cba0b312de28702a6c3c4d204f8c7b53e65cf
Leonardo Silva <103190146+leosilvafx@users.noreply.github.com>
2023-12-24 17:05:27 -0300

    ajuste final

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+//variaveis
+const screenOne = document.querySelector(".screenOne"),
+  screenTwo = document.querySelector(".screenTwo"),
+  btnTry = document.querySelector("#btnTry"),
+  btnAgain = document.querySelector("#btnAgain"),
+  inputNumber = document.querySelector("#inputNumber");
+
 let random = Math.floor(Math.random() * 5),
   kicks = 1;
 
-const screenOne = document.querySelector(".screenOne"),
-  screenTwo = document.querySelector(".screenTwo");
-
+//funcoes
 reloadClassBtns = () => {
   screenOne.classList.toggle("hide");
   screenTwo.classList.toggle("hide");
@@ -11,25 +16,15 @@ reloadClassBtns = () => {
 
 handleTryClick = (e) => {
   e.preventDefault();
-  const inputNumber = document.querySelector("#inputNumber");
 
   if (inputNumber.value == random) {
     reloadClassBtns();
 
-    document.querySelector(
-      ".screenTwo h2"
+    screenTwo.querySelector(
+      "h2"
     ).innerText = `Acertou em ${kicks} tentativa(s)`;
   } else kicks++;
 };
 
-gameAgain = () => {
-  e.preventDefault();
-  kicks = 1;
-  reloadClassBtns();
-};
-
-const btnTry = document.querySelector("#btnTry"),
-  btnAgain = document.querySelector("#btnAgain");
-
+//atribuicao de funcoes
 btnTry.addEventListener("click", handleTryClick);
-btnAgain.addEventListener("click", gameAgain);
